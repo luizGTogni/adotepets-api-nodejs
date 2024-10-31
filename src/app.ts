0c192401c1c7a2a2d8bb8bb1c3fdbfa3712fe1b0
Luiz Togni <trueluizbr@gmail.com>
2024-10-30 23:15:52 -0300

    add basic create orgs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,30 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import { z } from 'zod';
+import { prisma } from './lib/prisma';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post('/orgs', async (request, reply) => {
+  const requestBodySchema = z.object({
+    name: z.string(),
+    author_name: z.string(),
+    email: z.string(),
+    whatsapp: z.string(),
+    password: z.string(),
+    cep: z.string(),
+    state: z.string(),
+    city: z.string(),
+    neighborhood: z.string(),
+    street: z.string(),
+    latitude: z.number(),
+    longitude: z.number(),
+  });
+
+  const data = requestBodySchema.parse(request.body);
+
+  await prisma.oRG.create({
+    data,
+  });
+
+  return reply.status(201).send();
+});
