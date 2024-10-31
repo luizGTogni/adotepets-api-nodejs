3c24c3a8578075e4cda3f343482a830b3443856c
Luiz Togni <trueluizbr@gmail.com>
2024-10-30 22:54:38 -0300

    add prisma and postgres db

---
+++ prisma/migrations/20241031015339_create_pets_and_orgs_tables/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "size" TEXT NOT NULL,
+    "energy_level" TEXT NOT NULL,
+    "ord_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "author_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_ord_id_fkey" FOREIGN KEY ("ord_id") REFERENCES "orgs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
